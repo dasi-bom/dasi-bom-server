7338982ebfb96854e1f289d26ae34cf0909f04ab
minju412 <ln8847@ajou.ac.kr>
2023-06-14 02:18:23 +0900

    update HomeController

--- src/main/java/com/example/server/HomeController.java
+++ src/main/java/com/example/server/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping({"", "/"})
     public String home() {
-        return "home";
+        return "home22";
     }
 }
