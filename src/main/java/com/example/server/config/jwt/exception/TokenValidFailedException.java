8bbc8bebe03908f3bb5db8887372e8ccaf19ecfa
minju412 <ln8847@ajou.ac.kr>
2023-06-14 20:00:53 +0900

    JWT 적용 완료

---
+++ src/main/java/com/example/server/config/jwt/exception/TokenValidFailedException.java
@@ -0,0 +1,12 @@
+package com.example.server.config.jwt.exception;
+
+public class TokenValidFailedException extends RuntimeException {
+
+    public TokenValidFailedException() {
+        super("Failed to generate Token.");
+    }
+
+    public TokenValidFailedException(String message) {
+        super(message);
+    }
+}
