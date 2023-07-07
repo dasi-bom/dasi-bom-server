0944dffd15d8d4500de3028229103f34bbd5af1a
minju412 <ln8847@ajou.ac.kr>
2023-07-07 17:11:00 +0900

    feat: 회원 프로필 이미지 등록 api

--- src/main/java/com/example/server/service/MemberService.java
+++ src/main/java/com/example/server/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.example.server.service;
 
 import com.example.server.dto.MemberDto;
+import java.io.IOException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberService {
 
     void updateProfile(MemberDto.ProfileSaveRequest reqDto, UserDetails userDetails);
+    void uploadProfileImage(UserDetails userDetails, MultipartFile multipartFile, String dirName) throws IOException;
 }
--- src/main/java/com/example/server/service/MemberServiceImpl.java
+++ src/main/java/com/example/server/service/MemberServiceImpl.java
@@ -2,19 +2,23 @@ package com.example.server.service;
 
 import static com.example.server.exception.ErrorCode.MEMBER_NOT_FOUND;
 
+import com.example.server.domain.Image;
 import com.example.server.domain.Member;
 import com.example.server.dto.MemberDto;
 import com.example.server.exception.CustomException;
 import com.example.server.repository.MemberRepository;
+import java.io.IOException;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
+    private final S3Service s3Service;
     private final MemberRepository memberRepository;
 
     @Override
@@ -22,6 +26,15 @@ public class MemberServiceImpl implements MemberService {
     public void updateProfile(MemberDto.ProfileSaveRequest reqDto, UserDetails userDetails) {
         Member member = memberRepository.findByProviderId(userDetails.getUsername())
                 .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
-        member.updateProfile(reqDto.getNickname());
+        member.updateProfileInfo(reqDto.getNickname());
+    }
+
+    @Override
+    @Transactional
+    public void uploadProfileImage(UserDetails userDetails, MultipartFile multipartFile, String dirName) throws IOException {
+        Member member = memberRepository.findByProviderId(userDetails.getUsername())
+                .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+        Image img = s3Service.uploadSingleImage(multipartFile, dirName);
+        member.updateProfileImage(img);
     }
 }
