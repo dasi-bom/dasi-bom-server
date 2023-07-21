3d62b83be6e48322fcc1fb7dbf40075dc44c464f
Haebin <hbyeon@student.42seoul.kr>
2023-07-21 17:33:00 +0900

    feat: auto code reviewer

--- src/main/java/com/example/server/domain/Member.java
+++ src/main/java/com/example/server/domain/Member.java
@@ -26,33 +26,33 @@ import org.hibernate.annotations.CreationTimestamp;
 @AllArgsConstructor
 @Table(name = "member_tb")
 public class Member {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name; // 사용자명
-	private String username; // 아이디
-	private String password;
-	private String email;
-	private String mobile;
-	@Enumerated(EnumType.STRING)
-	private RoleType role;
-	@Enumerated(EnumType.STRING)
-	private OAuth2Provider provider;
-	private String providerId;
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "image_id")
-	private Image profileImage;
-	private String nickname;
-	@CreationTimestamp
-	private Timestamp createDate;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name; // 사용자명
+    private String username; // 아이디
+    private String password;
+    private String email;
+    private String mobile;
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+    @Enumerated(EnumType.STRING)
+    private OAuth2Provider provider;
+    private String providerId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id")
+    private Image profileImage;
+    private String nickname;
+    @CreationTimestamp
+    private Timestamp createDate;
 
-	public void updateProfileInfo(String nickname) {
-		if (StringUtils.isNotBlank(nickname)) {
-			this.nickname = nickname;
-		}
-	}
+    public void updateProfileInfo(String nickname) {
+        if (StringUtils.isNotBlank(nickname)) {
+            this.nickname = nickname;
+        }
+    }
 
-	public void updateProfileImage(Image image) {
-		this.profileImage = image;
-	}
+    public void updateProfileImage(Image image) {
+        this.profileImage = image;
+    }
 }
