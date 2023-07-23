3b2184842c88fabd398394323a613fd727be8279
Haebin <joker7011@kyonggi.ac.kr>
2023-07-23 14:05:32 +0900

    Update Member.java

--- src/main/java/com/example/server/domain/Member.java
+++ src/main/java/com/example/server/domain/Member.java
@@ -1,3 +1,31 @@
+package com.example.server.domain;
+
+import com.example.server.config.oauth.provider.OAuth2Provider;
+import com.nimbusds.oauth2.sdk.util.StringUtils;
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "member_tb")
+public class Member {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
