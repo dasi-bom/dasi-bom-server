9b837de4acb2c53d56f828bd9e240a363aed9a79
Haebin <joker7011@kyonggi.ac.kr>
2023-07-23 14:05:16 +0900

    fix: git conflict

--- src/main/java/com/example/server/domain/Member.java
+++ src/main/java/com/example/server/domain/Member.java
@@ -1,58 +1,30 @@
-package com.example.server.domain;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name; // 사용자명
+	private String username; // 아이디
+	private String password;
+	private String email;
+	private String mobile;
+	@Enumerated(EnumType.STRING)
+	private RoleType role;
+	@Enumerated(EnumType.STRING)
+	private OAuth2Provider provider;
+	private String providerId;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "image_id")
+	private Image profileImage;
+	private String nickname;
+	@CreationTimestamp
+	private Timestamp createDate;
 
-import com.example.server.config.oauth.provider.OAuth2Provider;
-import com.nimbusds.oauth2.sdk.util.StringUtils;
-import java.sql.Timestamp;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
+	public void updateProfileInfo(String nickname) {
+		if (StringUtils.isNotBlank(nickname)) {
+			this.nickname = nickname;
+		}
+	}
 
-@Builder
-@Data
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "member_tb")
-public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name; // 사용자명
-    private String username; // 아이디
-    private String password;
-    private String email;
-    private String mobile;
-    @Enumerated(EnumType.STRING)
-    private RoleType role;
-    @Enumerated(EnumType.STRING)
-    private OAuth2Provider provider;
-    private String providerId;
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "image_id")
-    private Image profileImage;
-    private String nickname;
-    @CreationTimestamp
-    private Timestamp createDate;
-
-    public void updateProfileInfo(String nickname) {
-        if (StringUtils.isNotBlank(nickname)) {
-            this.nickname = nickname;
-        }
-    }
-
-    public void updateProfileImage(Image image) {
-        this.profileImage = image;
-    }
+	public void updateProfileImage(Image image) {
+		this.profileImage = image;
+	}
 }
