c44065b5134e3be38a73021fbaf5b528b9ab6dd1
Haebin <hbyeon@student.42seoul.kr>
2023-07-24 13:38:01 +0900

    refactor : init CQS Pattern

--- src/main/java/com/example/server/service/MemberServiceImpl.java
+++ src/main/java/com/example/server/service/MemberServiceImpl.java
@@ -7,7 +7,7 @@ import com.example.server.domain.Image;
 import com.example.server.domain.Member;
 import com.example.server.dto.MemberDto;
 import com.example.server.exception.CustomException;
-import com.example.server.repository.MemberRepository;
+import com.example.server.repository.MemberQueryRepository;
 import com.nimbusds.oauth2.sdk.util.StringUtils;
 import java.io.IOException;
 import javax.transaction.Transactional;
@@ -21,12 +21,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class MemberServiceImpl implements MemberService {
 
     private final S3Service s3Service;
-    private final MemberRepository memberRepository;
+    private final MemberQueryRepository memberQueryRepository;
 
     @Override
     @Transactional
     public void updateProfile(MemberDto.ProfileSaveRequest reqDto, UserDetails userDetails) {
-        Member member = memberRepository.findByProviderId(userDetails.getUsername())
+        Member member = memberQueryRepository.findByProviderId(userDetails.getUsername())
                 .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
         if (StringUtils.isNotBlank(reqDto.getNickname())) {
             validateDuplicatedNickname(reqDto.getNickname());
@@ -37,14 +37,14 @@ public class MemberServiceImpl implements MemberService {
     @Override
     @Transactional
     public void uploadProfileImage(UserDetails userDetails, MultipartFile multipartFile, String dirName) throws IOException {
-        Member member = memberRepository.findByProviderId(userDetails.getUsername())
+        Member member = memberQueryRepository.findByProviderId(userDetails.getUsername())
                 .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
         Image img = s3Service.uploadSingleImage(multipartFile, dirName);
         member.updateProfileImage(img);
     }
 
     private void validateDuplicatedNickname(String nickname) {
-        if (memberRepository.existsByNickname(nickname)) {
+        if (memberQueryRepository.existsByNickname(nickname)) {
             throw new CustomException(CONFLICT_NICKNAME);
         }
     }
