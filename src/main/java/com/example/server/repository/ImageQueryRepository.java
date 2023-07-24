7d475331288b51bdc3479b1f8d87b9527d1a79ec
Haebin <joker7011@kyonggi.ac.kr>
2023-07-24 22:50:31 +0900

    refactor : init CQS Pattern (#55)

--- src/main/java/com/example/server/repository/ImageQueryRepository.java
+++ src/main/java/com/example/server/repository/ImageQueryRepository.java
@@ -1,13 +1,13 @@
 package com.example.server.repository;
 
-import com.example.server.domain.Member;
+import com.example.server.domain.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
-public interface MemberRepository extends JpaRepository<Member, Long> {
+@Transactional(readOnly = true)
+public interface ImageQueryRepository extends JpaRepository<Image, Long> {
 }
 
 
--- src/main/java/com/example/server/repository/ImageRepository.java
+++ src/main/java/com/example/server/repository/ImageRepository.java
@@ -2,7 +2,11 @@ package com.example.server.repository;
 
 import com.example.server.domain.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional
 public interface ImageRepository extends JpaRepository<Image, Long> {
 }
 
