6b3e74e08b9c1cfae28dcc61dd11881376f09b81
Haebin <joker7011@kyonggi.ac.kr>
2023-07-24 13:47:25 +0900

    feat: Refactor ErrorCode (#53)
    * feat: Refactor error response format
    
    * feat: Refactor error response format

--- src/main/java/com/example/server/config/jwt/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/example/server/config/jwt/handler/OAuth2AuthenticationFailureHandler.java
@@ -2,11 +2,13 @@ package com.example.server.config.jwt.handler;
 
 import static com.example.server.exception.ErrorCode.SOCIAL_LOGIN_ERROR;
 
-import com.example.server.exception.CustomException;
+import com.example.server.exception.BusinessException;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -20,6 +22,6 @@ public class OAuth2AuthenticationFailureHandler implements AuthenticationFailure
 //        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 //        response.getWriter().write("소셜 로그인 실패! 서버 로그를 확인해주세요.");
         log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
-        throw new CustomException(SOCIAL_LOGIN_ERROR);
+        throw new BusinessException(SOCIAL_LOGIN_ERROR);
     }
 }
