a7d0b4c9120efca25cc4013237a927c3ccd1a774
Haebin <hbyeon@student.42seoul.kr>
2023-07-24 12:55:17 +0900

    feat: Refactor error response format

--- src/main/java/com/example/server/exception/ErrorResponse.java
+++ src/main/java/com/example/server/exception/ErrorResponse.java
@@ -11,26 +11,32 @@ import static java.time.LocalDateTime.now;
 @Getter
 @Builder
 public class ErrorResponse {
-    private final String error;
+    private final int status;
     private final String message;
+    private final String code;
+    private final LocalDateTime timestamp;
 
     public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
         return ResponseEntity
-            .status(errorCode.getHttpStatus())
-            .body(ErrorResponse.builder()
-                .error(errorCode.getHttpStatus().name())
-                .message(errorCode.getMessage())
-                .build()
-            );
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getHttpStatus().value())
+                        .message(errorCode.getMessage())
+                        .code(errorCode.getCode())
+                        .timestamp(now())
+                        .build()
+                );
     }
 
-    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode, String defaultMessage) {
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode, String errorMessage) {
         return ResponseEntity
-            .status(errorCode.getHttpStatus())
-            .body(ErrorResponse.builder()
-                .error(errorCode.getHttpStatus().name())
-                .message(defaultMessage)
-                .build()
-            );
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getHttpStatus().value())
+                        .message(errorMessage)
+                        .code(errorCode.getCode())
+                        .timestamp(now())
+                        .build()
+                );
     }
 }
