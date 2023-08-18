76a52d7650873fa3af799cd7d23736b7d43dc4a7
minju412 <59405576+minju412@users.noreply.github.com>
2023-08-18 18:10:51 +0900

    Feature/84 - Fetch member and pet profiles together & Fetch member profile (#85)
    * feat: Add post-registration member info retrieval API
    
    * feat: Add member profile retrieval API
    
    * feat: Update FirstProfileCreateResponse
    
    * feat: Enhance registration response with additional details
    
    * refactor: Add constants to enhance code clarity
    
    * feat: Modify response content
    
    * feat: Merge changes
    
    * refactor: Assemble MemberProfileSaveResponse with assembler
    
    * refactor: Assemble MemberProfileResponse with assembler

--- src/main/java/com/example/server/domain/member/api/MemberController.java
+++ src/main/java/com/example/server/domain/member/api/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +17,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.server.domain.member.api.assembler.MemberProfileResponseAssembler;
+import com.example.server.domain.member.api.assembler.MemberProfileSaveResponseAssembler;
+import com.example.server.domain.member.api.dto.MemberProfileResponse;
 import com.example.server.domain.member.api.dto.MemberProfileSaveRequest;
+import com.example.server.domain.member.api.dto.MemberProfileSaveResponse;
+import com.example.server.domain.member.application.MemberFindService;
 import com.example.server.domain.member.application.MemberService;
+import com.example.server.domain.member.model.Member;
 import com.example.server.global.dto.ApiResponse;
 import com.example.server.global.exception.BusinessException;
 
@@ -29,23 +36,30 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 
 	private final MemberService memberService;
+	private final MemberFindService memberFindService;
+	private final MemberProfileSaveResponseAssembler memberProfileSaveResponseAssembler;
+	private final MemberProfileResponseAssembler memberProfileResponseAssembler;
 
 	@PatchMapping("/profile")
-	public ResponseEntity<Void> updateProfile(@RequestBody MemberProfileSaveRequest reqDto,
+	public ResponseEntity<MemberProfileSaveResponse> updateProfile(@RequestBody MemberProfileSaveRequest reqDto,
 		@AuthenticationPrincipal UserDetails userDetails) {
-		memberService.updateProfile(reqDto, userDetails.getUsername());
-		return ApiResponse.success(null);
+		Member member = memberService.updateProfile(reqDto, userDetails.getUsername());
+		return ApiResponse.success(memberProfileSaveResponseAssembler.toResponse(member));
 	}
 
 	@PostMapping(value = "/profile/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ResponseEntity<Void> uploadProfileImage(@RequestParam MultipartFile multipartFile,
+	public ResponseEntity<MemberProfileSaveResponse> uploadProfileImage(@RequestParam MultipartFile multipartFile,
 		@AuthenticationPrincipal UserDetails userDetails) throws IOException {
 		if (multipartFile == null) {
 			throw new BusinessException(FILE_NOT_EXIST_ERROR);
 		}
-		memberService.uploadProfileImage(userDetails.getUsername(), multipartFile);
-
-		return ApiResponse.success(null);
+		Member member = memberService.uploadProfileImage(userDetails.getUsername(), multipartFile);
+		return ApiResponse.success(memberProfileSaveResponseAssembler.toResponse(member));
 	}
 
+	@GetMapping("/profile")
+	public ResponseEntity<MemberProfileResponse> getProfile(@AuthenticationPrincipal UserDetails userDetails) {
+		Member member = memberFindService.findMemberByProviderId(userDetails.getUsername());
+		return ApiResponse.success(memberProfileResponseAssembler.toResponse(member));
+	}
 }
