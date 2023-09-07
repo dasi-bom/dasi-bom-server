9bf6bd07243947cee4c756b2113d62358d8897bb
minju412 <59405576+minju412@users.noreply.github.com>
2023-09-07 14:34:28 +0900

    refactor: Modify endpoint to receive JSON requests using DiarySaveRequest

--- src/main/java/com/example/server/domain/diary/api/DiaryController.java
+++ src/main/java/com/example/server/domain/diary/api/DiaryController.java
@@ -1,8 +1,9 @@
 package com.example.server.domain.diary.api;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,36 +32,36 @@ public class DiaryController {
 	private final DiaryService diaryService;
 	private final DiaryResponseAssembler diaryResponseAssembler;
 
-	// @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-	// public ResponseEntity<DiaryResponse> createDiary(
-	// 	@AuthenticationPrincipal UserDetails userDetails,
-	// 	@RequestPart @Valid DiarySaveRequest diarySaveRequest,
-	// 	@RequestPart(required = false) List<MultipartFile> multipartFiles
-	// ) throws IOException {
-	// 	Diary diary = diaryService.createDiary(userDetails.getUsername(), diarySaveRequest, multipartFiles);
-	// 	DiaryResponse response = diaryResponseAssembler.toResponse(diary);
-	// 	return ApiResponse.created(response);
-	// }
-
-	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
 	public ResponseEntity<DiaryResponse> createDiary(
 		@AuthenticationPrincipal UserDetails userDetails,
-		@RequestParam("petId") Long petId,
-		@RequestParam("category") String category,
-		@RequestParam("content") String content,
-		@RequestParam("stamps") String stamps,
-		@RequestParam("isPublic") Boolean isPublic,
+		@RequestPart @Valid DiarySaveRequest diarySaveRequest,
 		@RequestPart(required = false) List<MultipartFile> multipartFiles
 	) throws IOException {
-		DiarySaveRequest diarySaveRequest = new DiarySaveRequest();
-		diarySaveRequest.setPetId(petId);
-		diarySaveRequest.setCategory(category);
-		diarySaveRequest.setContent(content);
-		diarySaveRequest.setStamps(Arrays.asList(stamps.split(", "))); // 스탬프를 리스트로 변환
-		diarySaveRequest.setIsPublic(isPublic);
-
 		Diary diary = diaryService.createDiary(userDetails.getUsername(), diarySaveRequest, multipartFiles);
 		DiaryResponse response = diaryResponseAssembler.toResponse(diary);
 		return ApiResponse.created(response);
 	}
+
+	// @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	// public ResponseEntity<DiaryResponse> createDiary(
+	// 	@AuthenticationPrincipal UserDetails userDetails,
+	// 	@RequestParam("petId") Long petId,
+	// 	@RequestParam("category") String category,
+	// 	@RequestParam("content") String content,
+	// 	@RequestParam("stamps") String stamps,
+	// 	@RequestParam("isPublic") Boolean isPublic,
+	// 	@RequestPart(required = false) List<MultipartFile> multipartFiles
+	// ) throws IOException {
+	// 	DiarySaveRequest diarySaveRequest = new DiarySaveRequest();
+	// 	diarySaveRequest.setPetId(petId);
+	// 	diarySaveRequest.setCategory(category);
+	// 	diarySaveRequest.setContent(content);
+	// 	diarySaveRequest.setStamps(Arrays.asList(stamps.split(", "))); // 스탬프를 리스트로 변환
+	// 	diarySaveRequest.setIsPublic(isPublic);
+	//
+	// 	Diary diary = diaryService.createDiary(userDetails.getUsername(), diarySaveRequest, multipartFiles);
+	// 	DiaryResponse response = diaryResponseAssembler.toResponse(diary);
+	// 	return ApiResponse.created(response);
+	// }
 }
